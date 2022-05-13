831b33d9c3db41771fe3bfdb7f5cb9cde34c2a08
Kishorepani <kishorepani07@gmail.com>
2022-05-13 16:41:22 +0530

    added some comments to the add function

--- math.py
+++ math.py
@@ -1,7 +1,9 @@
 #This is what assigned me
 def add(x,y)
+#if a and y are both integers, then add them
 if(type(x)== int and type(y)==int):
 return x+y
+#otherwise return None
 return None
 
 #Tom needs to do this
